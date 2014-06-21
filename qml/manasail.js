0efc1b6b2d3798adb12dfdb5a4c14381c0c83ec2
Markus Törnqvist <mjt@fadconsulting.com>
2014-06-21 14:04:21 +0300

    Meh at js imports and locations

---
+++ qml/manasail.js
@@ -0,0 +1,12 @@
+/*
+ * Set the game type
+ */
+function setGameType(gameType) {
+    if (gameType == "limited") {
+        console.log("ltd");
+    } else if (gameType == "constructed") {
+        console.log("ltd");
+    } else {
+        console.error("Unknown game type", gameType);
+    }
+}
