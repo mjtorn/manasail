97e6bcff017367a7ad70c49c0d2e2dc83a013f8d
Markus Törnqvist <mjt@fadconsulting.com>
2014-06-21 16:13:52 +0300

    More methods

--- qml/manasail.js
+++ qml/manasail.js
@@ -22,6 +22,14 @@ var ManaSail = (function(formats, data) {
         'maxLandCount': null,
         'dflLandCount': null
     }
+    data.counts = {
+        'land': 0,
+        'white': 0,
+        'red': 0,
+        'green': 0,
+        'blue': 0,
+        'black': 0
+    }
 
     function setGameFormat(gameFormat) {
         data.format = data.formats[gameFormat];
@@ -34,9 +42,39 @@ var ManaSail = (function(formats, data) {
         return data.format;
     }
 
+    function setCount(key, count) {
+        data.counts[key] = count;
+    }
+    function getCounts() {
+        return data.counts;
+    }
+
+    // TODO: Do something
+    function calculate() {
+    }
+
+    // TODO: Display mana base, not counts
+    function display(label) {
+        var s = '';
+        var key = null;
+        var val = null;
+        for (key in data.counts) {
+            if (key != 'land') {
+                val = data.counts[key];
+                s += key + ': ' + val + '\n';
+            }
+        }
+
+        label.text = s;
+    }
+
     return {
         'setGameFormat': setGameFormat,
-        'getGameFormat': getGameFormat
+        'getGameFormat': getGameFormat,
+        'setCount': setCount,
+        'getCounts': getCounts,
+        'calculate': calculate,
+        'display': display,
     }
 }(gameFormats, {})); // Cannot assign to global scope so pass an array
 
